f637ecb1d8b41c12381f3f2e57f0611c0a9726c1
Heinz N. Gies <heinz@licenser.net>
2010-03-22 09:52:56 +0100

    * Merged Raynes improvments, thanks a lot

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject clj-sandbox "0.1.1-SNAPSHOT"
+(defproject clj-sandbox "0.1.3-SNAPSHOT"
   :description "Clojure library for sandboxed execution"
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]]
   :dev-dependencies [
+	[lein-clojars "0.5.0-SNAPSHOT"]
         [swank-clojure/swank-clojure "1.1.0"]])
