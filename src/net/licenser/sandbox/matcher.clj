ecebc3e74f64c19bc0f88bbbdd40bddf6e3166dc
Heinz N. Gies <heinz@licenser.net>
2010-03-25 00:50:14 +0100

    Renamed matchers to matcher.

--- src/net/licenser/sandbox/matcher.clj
+++ src/net/licenser/sandbox/matcher.clj
@@ -1,4 +1,4 @@
-(ns net.licenser.sandbox.matchers)
+(ns net.licenser.sandbox.matcher)
 
 (defn function-matcher
   "Creates a tester that whitelists functions."
@@ -27,6 +27,20 @@
   (fn [form]
     (if (= (type form) java.lang.Class)
       (map (partial isa? form) classes)
+      '())))
+
+(defn partial-namespace-matcher
+  "Creates a tester that whitelists all functions within a namespace and all namespaces under it."
+  [& namespaces]
+  (fn [form]
+    (cond
+     (= (type form) clojure.lang.Var)
+     (let [ns (str (ns-name (:ns (meta form))))]
+       (map #(zero? (.indexOf ns (str %))) namespaces))
+     (= (type form) java.lang.Class)
+     (let [ns (second (re-find #"^class (.*)\.\w+$" (str form)))]
+       (map #(zero? (.indexOf ns (str %))) namespaces))
+     true
       '())))
 
 (defn combine-matchers
