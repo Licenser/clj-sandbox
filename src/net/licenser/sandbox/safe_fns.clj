7cf16b57f0285a007505cecfdddeace2a4457ac3
Heinz N. Gies <heinz@licenser.net>
2010-03-29 13:20:14 +0200

    * Added read and binding save fns

--- src/net/licenser/sandbox/safe_fns.clj
+++ src/net/licenser/sandbox/safe_fns.clj
@@ -3,14 +3,14 @@
 
 (def safe-functions
      {:general-fns (function-matcher '= '== 'case 'if 'comment 'complement 'let 'constantly 
-				     'do 'loop* 'loop 'let* 'recur 'fn* 'fn? 'hash 
-				     'identical? 'macroexpand  'name 'not= 'partial 
-				     'trampoline 'new '. 'byte 'byte-array 'bytes)
+         'do 'loop* 'loop 'let* 'recur 'fn* 'fn? 'hash 
+         'identical? 'macroexpand  'name 'not= 'partial 
+         'trampoline 'new '. 'byte 'byte-array 'bytes)
 
       :array-fns (function-matcher 'into-array 'aget)
 
       :bit-fns (function-matcher 'bit-and-not 'bit-clear 'bit-flip 'bit-not 'bit-or 'bit-set
-				 'bit-shift-left 'bit-shift-right 'bit-test 'bit-xor)
+         'bit-shift-left 'bit-shift-right 'bit-test 'bit-xor)
 
       :chunk-fns (function-matcher 'chunked-seq? 'chunk-first 'chunk-rest)
 
@@ -19,31 +19,35 @@
       :string-fns (function-matcher 'subs 'str)
 
       :regex-fns (function-matcher 're-find 're-groups 're-matcher 're-matches 're-pattern 
-           're-seq)
+         're-seq)
       :meta-fns (function-matcher 'meta 'with-meta)
 
       :logic-fns (function-matcher 'and 'or 'false? 'not 'true?)
 
       :math-fns (combine-matchers 
       (function-matcher '* '/ '+ '- '< '> '<= '>= 'compare 'dec 'even? 'inc 'max 
-           'min 'mod 'neg? 'odd? 'pos? 'quot 'rand 'rand-int 'rem 
-           'zero?) 
+         'min 'mod 'neg? 'odd? 'pos? 'quot 'rand 'rand-int 'rem 
+         'zero?) 
      (namespace-matcher 'clojure.contrib.math))
 
       :unchecked-math-fns (function-matcher 'unchecked-inc)
 
       :list-fns (function-matcher 'map 'reduce 'count 'doall 'dorun 'doseq
-          'conj 'concat 'cons  'cycle 'interleave 'interpose 'into  
-          'partition 'reverse 'rseq 'seq 'sequence 'sort 'sort-by 
-          'split-at 'split-with 'subseq 'subvec 'tree-seq 'zipmap
-          'vec 'vector 'vector? 'distinct 'drop 'drop-last 'drop-while 
-          'empty 'filter 'not-any? 'not-empty 'not-every? 'remove 
-          'replace 'repeat 'iterate 'list 'list* 'repeatedly 'replicate
-          'range 'ffirst 'first 'fnext 'last 'next 'nfirst 'nnext 'nth 
-          'nthnext 'peek 'pop 'rest 'second 'take 'take-last 'take-nth 
-          'take-while 'contains? 'counted? 'empty? 'every? 'reversible?
-          'seq? 'some 'sorted? 'apply)
+         'conj 'concat 'cons  'cycle 'interleave 'interpose 'into  
+         'partition 'reverse 'rseq 'seq 'sequence 'sort 'sort-by 
+         'split-at 'split-with 'subseq 'subvec 'tree-seq 'zipmap
+         'vec 'vector 'vector? 'distinct 'drop 'drop-last 'drop-while 
+         'empty 'filter 'not-any? 'not-empty 'not-every? 'remove 
+         'replace 'repeat 'iterate 'list 'list* 'repeatedly 'replicate
+         'range 'ffirst 'first 'fnext 'last 'next 'nfirst 'nnext 'nth 
+         'nthnext 'peek 'pop 'rest 'second 'take 'take-last 'take-nth 
+         'take-while 'contains? 'counted? 'empty? 'every? 'reversible?
+         'seq? 'some 'sorted? 'apply)
+
+      :binding-fns (function-matcher 'push-thread-bindings 'pop-thread-bindings)
       
+      :read-fns (function-matcher 'read-string 'read)
+ 
       :set-fns (function-matcher 'disj 'dissoc 'assoc 'find 'get 'get-in 'hash-set 'hash-map 
          'key 'keys 'merge 'merge-with 'select-keys 'set 'set? 
          'update-in  'sorted-map 'sorted-map-by 'sorted-set)})
