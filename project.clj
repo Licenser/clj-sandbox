7d593c3d529c328ae1372e32a4cbb0b01c2381bc
Heinz N. Gies <heinz@licenser.net>
2010-04-09 09:52:56 +0200

    Updated version for clojars push. Added lein-search to dev-dependencies of the project.

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
-(defproject clj-sandbox "0.2.10-SNAPSHOT"
+(defproject clj-sandbox "0.2.11"
   :namespaces [net.licenser.sandbox net.licenser.sandbox.matcher net.licenser.sandbox.tester net.licenser.sandbox.jvm net.licenser.sandbox.save-fns]
   :description "Clojure library for sandboxed execution"
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]]
   :dev-dependencies [
 	[lein-clojars "0.5.0-SNAPSHOT"]
+	[lein-search/lein-search "0.1.0-SNAPSHOT"]
         [swank-clojure/swank-clojure "1.1.0"]
 	[leiningen/lein-swank "1.1.0"]])
