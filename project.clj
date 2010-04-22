23866961d7ffc3565c248ab9afe9949b7672fa07
Heinz N. Gies <heinz@licenser.net>
2010-04-22 11:00:57 +0200

    Updated the version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-sandbox "0.3.0"
+(defproject clj-sandbox "0.3.1"
   :description "Clojure library for sandboxed execution"
   :dependencies [[org.clojure/clojure "[1.1,1.2]"]
                  [org.clojure/clojure-contrib "[1.1,1.2]"]]
