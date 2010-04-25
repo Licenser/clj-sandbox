0b704e3688568f0dc06456dfe7769b4ec6abcaf4
Rayne <DiscipleRayne@gmail.com>
2010-04-25 04:33:01 -0500

    Fixed deps.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject clj-sandbox "0.3.1"
+(defproject clj-sandbox "0.3.2"
   :description "Clojure library for sandboxed execution"
-  :dependencies [[org.clojure/clojure "[1.1,1.2]"]
-                 [org.clojure/clojure-contrib "[1.1,1.2]"]]
+  :dependencies [[org.clojure/clojure "[1.1,1.2-master-SNAPSHOT]"]
+                 [org.clojure/clojure-contrib "[1.1,1.2-SNAPSHOT]"]]
   :dev-dependencies [
 	[lein-clojars "0.5.0-SNAPSHOT"]
 	[lein-search "0.2.0"]
