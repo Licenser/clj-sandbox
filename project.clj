741229933963085fe9dfb71d25276065eaea5fab
Raynes <DiscipleRayne@gmail.com>
2010-07-20 22:03:15 -0500

    Got rid of redundant version range.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clj-sandbox "0.4.0"
   :description "Clojure library for sandboxed execution"
-  :dependencies [[org.clojure/clojure "[1.1,1.2-master-SNAPSHOT]"]
-                 [org.clojure/clojure-contrib "[1.1,1.2-SNAPSHOT]"]]
+  :dependencies [[org.clojure/clojure "1.1.0"]
+                 [org.clojure/clojure-contrib "1.1.0"]]
   :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
 		     [swank-clojure "1.2.1"]])
