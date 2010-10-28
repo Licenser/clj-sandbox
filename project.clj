5e05e5b1ff5d66db42fbb1aeff38768c886ee2e2
Raynes <DiscipleRayne@gmail.com>
2010-10-28 12:41:34 -0500

    Got rid of the mostly deprecated lein-clojars.

--- project.clj
+++ project.clj
@@ -2,5 +2,4 @@
   :description "Clojure library for sandboxed execution"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[lein-clojars "0.5.0"]
-		     [swank-clojure "1.2.1"]])
+  :dev-dependencies [[swank-clojure "1.2.1"]])
